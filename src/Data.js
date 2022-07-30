46017ecd8a22348833c902a030ff0833412202d9
Kevin Matthews <kvnrmatthews@gmail.com>
2022-07-30 14:51:18 -0700

    Add data for Richmond Summer Open A 2022
    B has one less group of sq-1

--- src/Data.js
+++ src/Data.js
@@ -10,13 +10,30 @@ export const groups = [
 
   { event: Event.e33, group: 1 },
   { event: Event.e33, group: 2 },
+  { event: Event.e33, group: 3 },
 
   { event: Event.etb },
 
   { event: Event.elu },
 
   { event: Event.esq, group: 1 },
-  { event: Event.esq, group: 2 },
+
+  { event: Event.e3o, group: 1 },
+  { event: Event.e3o, group: 2 },
+
+  { event: Event.e33, round: 2, who: "14th to 26th" },
+  { event: Event.e33, round: 2, who: "1st to 13th" },
+
+  { event: Event.e55, group: 1 },
+  { event: Event.e55, group: 2 },
+
+  { event: Event.e3o, round: 2, who: "Top 14" },
+
+  { event: Event.esk, round: 2, who: "Top 14" },
+
+  { event: Event.esq, round: 2, who: "Top 8" },
+
+  { event: Event.e33, round: 2, who: "Top 14" },
 
   { event: Event.eaw },
 
